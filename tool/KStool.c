6bb11faf2fbd5ce7465f98ead7fc956075850b48
valera disgrace <djdisgrace@protonmail.com>
2025-03-12 15:21:52 +1000

    userspace tool

---
+++ tool/KStool.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
+#define PROCFS_PATH "/proc/secret"
+#define MAX_SECRET_SIZE 1024
+
+void usage(const char *prog_name) {
+    fprintf(stderr, "usage: %s -d | -r | -w\n", prog_name);
+    fprintf(stderr, "  -d   delete the secret from memory\n");
+    fprintf(stderr, "  -r   read the secret from memory\n");
+    fprintf(stderr, "  -w   write the secret to memory from stdin\n");
+}
+
+void write_secret() {
+    char secret[MAX_SECRET_SIZE];
+    printf("enter the secret: \n");
+    if (!fgets(secret, MAX_SECRET_SIZE, stdin)) {
+        perror("failed to read secret");
+        exit(EXIT_FAILURE);
+    }
+    secret[strcspn(secret, "\n")] = 0;  // Remove newline character
+
+    int fd = open(PROCFS_PATH, O_WRONLY);
+    if (fd < 0) {
+        perror("failed to open proc file");
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(fd, secret, strlen(secret)) < 0) {
+        perror("failed to write secret\n");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+}
+
+void read_secret() {
+    int fd = open(PROCFS_PATH, O_RDONLY);
+    if (fd < 0) {
+        perror("failed to open proc file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[MAX_SECRET_SIZE];
+    ssize_t bytes_read = read(fd, buffer, MAX_SECRET_SIZE);
+    if (bytes_read < 0) {
+        perror("failed to read secret\n");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[bytes_read] = '\0';
+    printf("secret: %s\n", buffer);
+
+    close(fd);
+}
+
+void delete_secret() {
+    int fd = open(PROCFS_PATH, O_WRONLY);
+    if (fd < 0) {
+        perror("failed to open proc file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(fd, "", 0) < 0) {
+        perror("failed to delete secret\n");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int opt;
+    while ((opt = getopt(argc, argv, "drw")) != -1) {
+        switch (opt) {
+            case 'd':
+                delete_secret();
+                break;
+            case 'r':
+                read_secret();
+                break;
+            case 'w':
+                write_secret();
+                break;
+            default:
+                usage(argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
